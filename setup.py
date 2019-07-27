8f65a523665d11356c83ac1153daa655329abfe8
Panagiotis Matigakis <pmatigakis@gmail.com>
2019-07-27 21:53:44 +0300

    Added the requirement files and the license

--- setup.py
+++ setup.py
@@ -1,24 +1,30 @@
 from setuptools import setup, find_packages
 
 
+def read_requirements(filename):
+    with open(filename) as f:
+        return [
+            line.strip()
+            for line in f
+        ]
+
+
+def read_long_description():
+    with open("README.md") as f:
+        return f.read()
+
+
 setup(
     name="hackernewslib",
     version="0.1.0",
     author="Panagiotis Matigakis",
     author_email="pmatigakis@gmail.com",
     description="Hackernews client library",
-    packages=find_packages(exclude=["tests"]),
-    install_requires=[
-        "newspaper3k==0.2.8",
-        "marshmallow==2.19.2",
-        "python-firebase==1.2"
-    ],
-    setup_requires=[
-        "pytest-runner==4.4"
-    ],
-    tests_require=[
-        "pytest==4.4.1"
-    ],
+    long_description=read_long_description(),
+    packages=find_packages(include=["hackernewslib"]),
+    install_requires=read_requirements("requirements.txt"),
+    setup_requires=read_requirements("requirements-setup.txt"),
+    tests_require=read_requirements("requirements-test.txt"),
     include_package_data=True,
     zip_safe=False
 )
